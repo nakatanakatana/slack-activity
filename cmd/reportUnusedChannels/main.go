de5ab194325ea1da383df72e1bfca750a98ef9bf
tanaka <nakatanakatana@gmail.com>
2022-06-03 13:28:02 +0900

    reportUnusedChannel: run report parallel

--- cmd/reportUnusedChannels/main.go
+++ cmd/reportUnusedChannels/main.go
@@ -3,13 +3,19 @@ package main
 import (
 	"log"
 	"os"
+	"sync"
 
 	slackactivity "github.com/nakatanakatana/slack-activity"
 	"github.com/nakatanakatana/slack-activity/report"
 )
 
+const (
+	reportParallelLimit = 10
+)
+
 func _main() int {
 	cfg := report.CreateConfig()
+
 	err := report.CreateTmpDir(cfg)
 	if err != nil {
 		log.Println(err)
@@ -35,15 +41,30 @@ func _main() int {
 
 	log.Println("targetChannels", len(channels))
 
+	slots := make(chan struct{}, reportParallelLimit)
+
+	var wg sync.WaitGroup
+
+	wg.Add(len(channels))
+
 	for _, channel := range channels {
 		c := channel
 		log.Println("target channel:", c.Name)
 
-		err := report.SendChannelReport(c, cfg, ts, api, api)
-		if err != nil {
-			log.Println(err)
-		}
+		slots <- struct{}{}
+
+		go func() {
+			err := report.SendChannelReport(c, cfg, ts, api, api)
+			if err != nil {
+				log.Println(err)
+			}
+
+			<-slots
+			wg.Done()
+		}()
 	}
+
+	wg.Wait()
 
 	return 0
 }
